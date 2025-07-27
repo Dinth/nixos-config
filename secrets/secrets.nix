5206398fc5fa94e51ef357ace7a1f4bb868cd5aa
Michal Gawronski-Kot <michal@gawronskikot.com>
2025-07-27 10:52:53 +0100

    first commit

---
+++ secrets/secrets.nix
@@ -0,0 +1,12 @@
+let
+  michal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINnJL7HYauYQWLSdKDZwGJBj/OWu+rBZEcaxS/Dn/Wtq";
+  dinth-nixos-desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOhWUYbCP4MBoRhT5rSes+GCx27h1fd7yEjAAmGfnN1Y";
+
+  users = [ michal ];
+  systems = [ dinth-nixos-desktop ];
+in
+{
+  "michal-password.age".publicKeys = users ++ systems;
+  "wifi-password.age".publicKeys = systems;
+  "api-key.age".publicKeys = users ++ systems;
+}
