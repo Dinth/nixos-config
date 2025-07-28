b673db3e20d156f17847de8b24739d8252e48df0
Michal Gawronski-Kot <michal@gawronskikot.com>
2025-07-28 21:09:18 +0100

    added mime declarations, moved xdg.mimeApps to a new file

--- modules/home-manager/mime.nix
+++ modules/home-manager/mime.nix
@@ -1,37 +1,5 @@
-{config, pkgs, inputs,lib, ...}:
-let
-  catppuccin_konsole = pkgs.fetchFromGitHub {
-    owner = "catppuccin";
-    repo = "konsole";
-    rev = "3b64040e3f4ae5afb2347e7be8a38bc3cd8c73a8";
-    hash = "sha256-d5+ygDrNl2qBxZ5Cn4U7d836+ZHz77m6/yxTIANd9BU=";
-  };
-in
+{ config, lib, pkgs, ... }:
 {
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
-    };
-  };
-  home = {
-    stateVersion = "25.05";
-    username = "michal";
-    homeDirectory = "/home/michal";
-    packages = with pkgs; [
-      mqtt-explorer
-      discord
-      orca-slicer
-      signal-desktop
-      weechat
-    ];
-  };
-  imports = [
-    ./mc.nix
-    ./kde.nix
-    ./shell.nix
-    ./mime.nix
-  ];
-
   home.xdg.dataFile."mime/packages/x-plist.xml".text = ''
     <?xml version="1.0" encoding="UTF-8"?>
     <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
@@ -61,10 +29,4 @@ in
       "application/x-applescript" = "Kate.desktop";
     };
   };
-
-  catppuccin = {
-    enable = true;
-    flavor = "mocha";
-  };
-
 }
