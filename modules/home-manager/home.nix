17a7e98fa1f7e3acbb272b0bddde832ecd8823fd
Michal Gawronski-Kot <michal@gawronskikot.com>
2025-07-28 21:38:17 +0100

    cosmetics with mime

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -32,36 +32,6 @@ in
     ./mime.nix
   ];
 
-  home.xdg.dataFile."mime/packages/x-plist.xml".text = ''
-    <?xml version="1.0" encoding="UTF-8"?>
-    <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
-      <mime-type type="application/x-plist">
-        <comment>Apple Property List</comment>
-        <glob pattern="*.plist"/>
-      </mime-type>
-    </mime-info>
-  '';
-  home.xdg.dataFile."mime/packages/x-applescript.xml".text = ''
-    <?xml version="1.0" encoding="UTF-8"?>
-    <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
-      <mime-type type="text/x-applescript">
-        <comment>AppleScript Source</comment>
-        <glob pattern="*.applescript"/>
-      </mime-type>
-    </mime-info>
-  '';
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "x-scheme-handler/orcaslicer" = "OrcaSlicer.desktop";
-      "x-scheme-handler/bambustudio" = "OrcaSlicer.desktop"; # makerworld
-      "x-scheme-handler/prusaslicer" = "OrcaSlicer.desktop"; # printables
-      "x-scheme-handler/applescript" = "Kate.desktop"; # applescript:// urls
-      "application/x-plist" = "Kate.desktop"; # mac plist files
-      "application/x-applescript" = "Kate.desktop";
-    };
-  };
-
   catppuccin = {
     enable = true;
     flavor = "mocha";
--- modules/home-manager/mime.nix
+++ modules/home-manager/mime.nix
@@ -1,33 +1,37 @@
 { config, lib, pkgs, ... }:
 {
-  xdg.enable = true;
-  xdg.dataFile."mime/packages/x-plist.xml".text = ''
-    <?xml version="1.0" encoding="UTF-8"?>
-    <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
-      <mime-type type="application/x-plist">
-        <comment>Apple Property List</comment>
-        <glob pattern="*.plist"/>
-      </mime-type>
-    </mime-info>
-  '';
-  xdg.dataFile."mime/packages/x-applescript.xml".text = ''
-    <?xml version="1.0" encoding="UTF-8"?>
-    <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
-      <mime-type type="text/x-applescript">
-        <comment>AppleScript Source</comment>
-        <glob pattern="*.applescript"/>
-      </mime-type>
-    </mime-info>
-  '';
-  xdg.mimeApps = {
+  xdg = {
     enable = true;
-    defaultApplications = {
-      "x-scheme-handler/orcaslicer" = "OrcaSlicer.desktop";
-      "x-scheme-handler/bambustudio" = "OrcaSlicer.desktop"; # makerworld
-      "x-scheme-handler/prusaslicer" = "OrcaSlicer.desktop"; # printables
-      "x-scheme-handler/applescript" = "Kate.desktop"; # applescript:// urls
-      "application/x-plist" = "Kate.desktop"; # mac plist files
-      "text/x-applescript" = "Kate.desktop"; # applescript
+    dataFile = {
+      "mime/packages/x-plist.xml".text = ''
+        <?xml version="1.0" encoding="UTF-8"?>
+        <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
+          <mime-type type="application/x-plist">
+            <comment>Apple Property List</comment>
+            <glob pattern="*.plist"/>
+          </mime-type>
+        </mime-info>
+      '';
+      "mime/packages/x-applescript.xml".text = ''
+        <?xml version="1.0" encoding="UTF-8"?>
+        <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
+          <mime-type type="text/x-applescript">
+            <comment>AppleScript Source</comment>
+            <glob pattern="*.applescript"/>
+          </mime-type>
+        </mime-info>
+      '';
+    };
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "x-scheme-handler/orcaslicer" = "OrcaSlicer.desktop";
+        "x-scheme-handler/bambustudio" = "OrcaSlicer.desktop"; # makerworld
+        "x-scheme-handler/prusaslicer" = "OrcaSlicer.desktop"; # printables
+        "x-scheme-handler/applescript" = "Kate.desktop"; # applescript:// urls
+        "application/x-plist" = "Kate.desktop"; # mac plist files
+        "text/x-applescript" = "Kate.desktop"; # applescript
+      };
     };
   };
 }
