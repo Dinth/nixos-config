e4b8bab61d91d28ddf940b8976ed063b6d3fae20
Michal Gawronski-Kot <michal@gawronskikot.com>
2025-08-12 09:02:16 +0100

    r230-nixos prep

--- hosts/r230-nixos/configuration.nix
+++ hosts/r230-nixos/configuration.nix
@@ -11,9 +11,34 @@
     name = "michal";
     fullName = "Michal Gawronski-Kot";
     email = "michal@gawronskikot.com";
+    publicKeys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINnJL7HYauYQWLSdKDZwGJBj/OWu+rBZEcaxS/Dn/Wtq"
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHw58iDAXminEmYKnzUjRzMhpR7rvULZZUZ0izMdiuhSAAAABHNzaDo="
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIK+KGs2KSEQaHkzK+awc4QXMKu6kMn10F7cZ4raPcQJKAAAABHNzaDo="
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMOPDiAQbAD53X2neUh/vbIv7pRx2+qkZ7Ti9PH+CJ1yAAAABHNzaDo="
+    ];
   };
   system.stateVersion = "25.11";
-
+  users.users.${config.primaryUser.name} = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    description = config.primaryUser.fullName;
+    extraGroups = [ "networkmanager" "wheel" "scanner" "network" "disk" "audio" "video" "vboxusers" "dialout" "gamemode" "scanner" "lp" ];
+    openssh.authorizedKeys.keys = config.primaryUser.publicKeys;
+  };
+  home-manager.users.${config.primaryUser.name} = {
+    home = {
+      stateVersion = "25.05";
+      username = config.primaryUser.name;
+      homeDirectory = "/home/${config.primaryUser.name}";
+      packages = with pkgs; [
+        mqtt-explorer
+        discord
+        signal-desktop
+      ];
+    };
+    catppuccin.flavor = "mocha";
+  };
   cli.enable = true;
   graphical.enable = false;
   kde.enable = false;
--- hosts/r230-nixos/hardware-configuration.nix
+++ hosts/r230-nixos/hardware-configuration.nix
@@ -1,7 +1,12 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
+  services.qemuGuest.enable = true;
+
+  boot.loader.grub.enable = true; # Use the boot drive for GRUB
+  boot.loader.grub.devices = [ "nodev" ];
+
 }
