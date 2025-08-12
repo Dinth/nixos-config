7af08761fe803238fc8db158c5d67978ae5cd3bf
Michal Gawronski-Kot <michal@gawronskikot.com>
2025-08-12 18:11:50 +0100

    r230-nixos prep

--- hosts/common.nix
+++ hosts/common.nix
@@ -6,10 +6,6 @@
     };
   };
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.timeout = 1;
-  boot.initrd.systemd.enable = true;
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 #  boot.tmp.cleanOnBoot = true;
