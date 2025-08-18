4c642f5206064477ddd4a80869fa338287a60316
Michal Gawronski-Kot <michal@gawronskikot.com>
2025-08-18 08:15:58 +0100

    added basic config for Surface Go

--- modules/apps/google-chrome/default.nix
+++ modules/apps/google-chrome/default.nix
@@ -9,23 +9,10 @@ in
     environment.systemPackages = with pkgs; [
       google-chrome
     ] ++ lib.optionals (machineType == "tablet") [
-        (
-          google-chrome.override {
-            extraFlags = [
-              "--enable-features=TouchpadOverscrollHistoryNavigation"
-              "--enable-features=VaapiVideoDecoder"
-              "--ozone-platform=wayland"
-              "--enable-wayland-ime"
-              "--touch-events=enabled"
-              "--force-device-scale-factor=1.25"
-              "--enable-smooth-scrolling"
-              "--enable-gpu-rasterization"
-              "--enable-hardware-overlays"
-              "--enable-zero-copy"
-            ];
-          }
-        )
-      ];
+      (google-chrome.override {
+        commandLineArgs = "--enable-features=TouchpadOverscrollHistoryNavigation --enable-features=VaapiVideoDecoder --ozone-platform=wayland --enable-wayland-ime --touch-events=enabled --force-device-scale-factor=1.25 --enable-smooth-scrolling --enable-gpu-rasterization --enable-hardware-overlays --enable-zero-copy";
+      })
+    ];
     environment.etc."/opt/chrome/policies/enrollment/CloudManagementEnrollmentToken".source = config.age.secrets.chrome-enrolment.path;
     environment.etc."/opt/chrome/policies/enrollment/CloudManagementEnrollmentOptions".text = "Mandatory";
     environment.sessionVariables.NO_AT_BRIDGE = "1";
