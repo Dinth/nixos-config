752f856a72b5934cf68f7132488fa5818e25970e
Michal Gawronski-Kot <michal@gawronskikot.com>
2025-08-19 21:20:56 +0100

    agenix key update

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,8 @@
 let
   michal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINnJL7HYauYQWLSdKDZwGJBj/OWu+rBZEcaxS/Dn/Wtq";
   dinth-nixos-desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOhWUYbCP4MBoRhT5rSes+GCx27h1fd7yEjAAmGfnN1Y";
+  michal-surface-go = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOgUEaVYhvpUJMCyycsqmilyZMfVHOK/EFh4nlNaC9yi";
+  r230-nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEHxcwQf5t5dnvgvNX7bCN0t9jmuBBG3YC6Z0fsryusW";
 
   users = [ michal ];
   systems = [ dinth-nixos-desktop michal-surface-go r230-nixos ];
