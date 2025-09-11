231bfa10a31cd38199db515591944a71a475bfc2
Michal Gawronski-Kot <michal@gawronskikot.com>
2025-09-11 20:42:22 +0100

    experimenting with ddc controls for eizo screen

--- hosts/dinth-nixos-desktop/hardware-configuration.nix
+++ hosts/dinth-nixos-desktop/hardware-configuration.nix
@@ -153,4 +153,5 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   amd_gpu.enable = true;
+  eizo.enable = true;
 }
