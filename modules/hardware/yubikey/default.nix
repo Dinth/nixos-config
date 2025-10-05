174bcfb5aec88b590d308ff871765f28da2a941b
Michal Gawronski-Kot <michal@gawronskikot.com>
2025-10-05 12:24:58 +0100

    apparently yubioath is now fixed, workaround can be remove

--- modules/hardware/yubikey/default.nix
+++ modules/hardware/yubikey/default.nix
@@ -3,11 +3,6 @@ let
   inherit (lib) mkIf mkOption mkMerge;
   cfg = config.yubikey;
   primaryUsername = config.primaryUser.name;
-  # Enforcing yubikey-manager 5.7.1 - required for yubioath-flutter <=7.2.3
-  oldNixpkgs = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/a343533bccc62400e8a9560423486a3b6c11a23b.tar.gz";
-    sha256 = "0103a1a1g5sp4bjhm6fl0nfw69jgdiwrwz96nnqi0f3bg6vcg1sf";  # Leave empty first to get hash from error
-  }) {};
 in
 {
   options = {
@@ -25,7 +20,7 @@ in
       # Enforcing yubikey-manager 5.7.1 - required for yubioath-flutter <=7.2.3
       environment.systemPackages = [
         pkgs.libfido2
-        oldNixpkgs.yubikey-manager
+        yubikey-manager
       ];
 #      environment.systemPackages = with pkgs; [
 #        libfido2 # FIDO2 library (for Yubikeys)
