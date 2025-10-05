86f769766ad4d1fef5ffd2483fa5abebff2ea619
Michal Gawronski-Kot <michal@gawronskikot.com>
2025-10-05 12:25:56 +0100

    apparently yubioath is now fixed, workaround can be removed

--- modules/hardware/yubikey/default.nix
+++ modules/hardware/yubikey/default.nix
@@ -17,15 +17,10 @@ in
   config = mkMerge [
     (mkIf cfg.enable {
       services.pcscd.enable = true;
-      # Enforcing yubikey-manager 5.7.1 - required for yubioath-flutter <=7.2.3
-      environment.systemPackages = [
-        pkgs.libfido2
-        yubikey-manager
+      environment.systemPackages = with pkgs; [
+        libfido2 # FIDO2 library (for Yubikeys)
+        yubikey-manager # Yubikey manager
       ];
-#      environment.systemPackages = with pkgs; [
-#        libfido2 # FIDO2 library (for Yubikeys)
-#        yubikey-manager # Yubikey manager
-#      ];
     })
     (mkIf (cfg.enable && config.graphical.enable) {
       environment.systemPackages = with pkgs; [
