84398b8e2f2e11f687c84ba056f435b462b3747d
Michal Gawronski-Kot <michal@gawronskikot.com>
2025-11-27 21:02:37 +0000

    added some chrome flags

--- modules/apps/google-chrome/default.nix
+++ modules/apps/google-chrome/default.nix
@@ -11,7 +11,7 @@ in
       google-chrome
     ] ++ lib.optionals (machineType == "tablet") [
       (google-chrome.override {
-        commandLineArgs = "--enable-features=TouchpadOverscrollHistoryNavigation --enable-features=VaapiVideoDecoder --ozone-platform=wayland --enable-wayland-ime --touch-events=enabled --force-device-scale-factor=1.25 --enable-smooth-scrolling --enable-gpu-rasterization --enable-hardware-overlays --enable-zero-copy";
+        commandLineArgs = "--enable-features=TouchpadOverscrollHistoryNavigation --enable-features=VaapiVideoDecoder --ozone-platform=wayland --enable-wayland-ime --enable-features=ScollableTabStrip --enable-features=ParallelDownloading --enable-gpu-rasterization --enable-zero-copy --touch-events=enabled --force-device-scale-factor=1.25 --enable-smooth-scrolling --enable-gpu-rasterization --enable-hardware-overlays --enable-zero-copy";
       })
     ];
     environment.etc."/opt/chrome/policies/enrollment/CloudManagementEnrollmentToken".source = config.age.secrets.chrome-enrolment.path;
