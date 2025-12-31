993ed052581801e196d86f079111265914bd9a2e
Michal Gawronski-Kot <michal@gawronskikot.com>
2025-12-31 15:24:25 +0000

    zsh improvements

--- modules/apps/zsh/default.nix
+++ modules/apps/zsh/default.nix
@@ -1,20 +1,21 @@
-{ config, lib, pkgs,...}:
+{ config, lib, pkgs, ... }:
 let
-  inherit (lib) mkIf;
+  inherit (lib) mkIf getExe getExe';
   cfg = config.cli;
   primaryUsername = config.primaryUser.name;
-in
-{
+in {
   config = mkIf cfg.enable {
-    programs.zsh = {
-      enable = true;
-    };
-    home-manager.users.${primaryUsername} = {
+    # System level: minimal
+    programs.zsh.enable = true;
+
+    # Home-manager: all user config here
+    home-manager.users.${primaryUsername} = { config, ... }: {
       programs.zsh = {
         enable = true;
         enableCompletion = true;
         autosuggestion.enable = true;
         syntaxHighlighting.enable = true;
+
         history = {
           size = 100000;
           save = 100000;
@@ -26,62 +27,61 @@ in
           ignoreAllDups = true;
           saveNoDups = true;
         };
+
         shellAliases = {
-          cat = "${lib.getExe pkgs.bat}";
-          ls = "${lib.getExe pkgs.eza} -l";
-          tree = "${lib.getExe pkgs.eza} --tree --all";
-          top = "${lib.getExe pkgs.btop}";
+          cat = "${getExe pkgs.bat}";
+          ls = "${getExe pkgs.eza} -l";
+          tree = "${getExe pkgs.eza} --tree --all";
+          top = "${getExe pkgs.btop}";
         };
+
+        # Use initExtra (appends) instead of initContent (replaces)
         initContent = ''
-          function lscontent {
-            ${lib.getExe pkgs.tree} -I 'node_modules|.git'
-            ${lib.getExe' pkgs.coreutils "printf"} "\n--- FILE CONTENTS ---\n\n"
-            ${lib.getExe pkgs.findutils} . -type f \
-            -not -path '*/.git/*' \
-            -not -path '*/node_modules/*' \
-            -not -path '*/flake.lock' \
-            -exec ${lib.getExe' pkgs.bash "sh"} -c '
-            # For each file found by find, do the following:
-              ${lib.getExe' pkgs.coreutils "echo"} "--- FILE: {} ---"
-              ${lib.getExe' pkgs.coreutils "cat"} "{}"
-              ${lib.getExe' pkgs.coreutils "echo"}
-            ' \;
+          setopt EXTENDED_HISTORY HIST_SAVE_NO_DUPS INC_APPEND_HISTORY
+          unsetopt SHARE_HISTORY
+
+          export LS_COLORS="$(${getExe pkgs.vivid} generate catppuccin-mocha)"
+
+          function lscontent() {
+            ${getExe pkgs.tree} -I 'node_modules|.git' "''${@:-.}"
+            echo ""
+            echo "--- FILE CONTENTS ---"
+            echo ""
+            ${getExe pkgs.findutils} "''${@:-.}" -type f \
+              -not -path '*/.git/*' \
+              -not -path '*/node_modules/*' \
+              -not -path '*/flake.lock' \
+              -exec sh -c '
+                echo "--- FILE: {} ---"
+                ${getExe pkgs.bat} --plain "{}" 2>/dev/null || cat "{}"
+              ' \;
           }
-        '' +
-          ( lib.optionalString (pkgs.zoxide != null) ''
-              function cd {
-                if [[ -n "$MC_SID" ]]; then
-                  builtin cd "$@"
-                else
-                  z "$@"
-                fi
-              }
-            ''
-          ) +
-          ( lib.optionalString (pkgs.kdePackages.plasma-workspace != null) ''
-              function pbcopy {
-                ${lib.getExe' pkgs.kdePackages.qttools "qdbus"} org.kde.klipper /klipper setClipboardContents "$(${lib.getExe' pkgs.coreutils "cat"} "$@")"
-              }
-            ''
-          ) +
-        ''
-          export LS_COLORS="$(${lib.getExe pkgs.vivid} generate catppuccin-mocha)"
 
-          # Manual Konsole semantic shell integration
+          ${lib.optionalString (lib.hasAttr "zoxide" pkgs) ''
+            function cd() {
+              if [[ -n "$MC_SID" ]]; then
+                builtin cd "$@"
+              else
+                z "$@"
+              fi
+            }
+          ''}
+
+          ${lib.optionalString (lib.hasAttr "plasma-workspace" pkgs.kdePackages) ''
+            function pbcopy() {
+              ${getExe' pkgs.kdePackages.qttools "qdbus"} \
+                org.kde.klipper /klipper setClipboardContents \
+                "$(cat "$@")"
+            }
+          ''}
+
           if [[ -n "$KONSOLE_DBUS_SESSION" ]]; then
-            # Add semantic zone markers to prompt
-            # These escape sequences mark prompt, input, and output regions
-
-            precmd() {
-              print -n $'\e]133;A\e\\' # Mark prompt start
-            }
-
-            preexec() {
-              print -n $'\e]133;C\e\\' # Mark command start
-            }
+            precmd() { print -n $'\e]133;A\e\\'; }
+            preexec() { print -n $'\e]133;C\e\\'; }
           fi
         '';
       };
+
       programs.starship = {
         enable = true;
         enableZshIntegration = true;
