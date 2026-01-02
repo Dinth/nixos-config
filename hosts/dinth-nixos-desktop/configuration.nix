f9284f907c55c736854928656d679828262d8fe1
Michal Gawronski-Kot <michal@gawronskikot.com>
2026-01-02 11:30:04 +0000

    dbus broker enabled

--- hosts/dinth-nixos-desktop/configuration.nix
+++ hosts/dinth-nixos-desktop/configuration.nix
@@ -97,7 +97,10 @@
       "x-systemd.mount-timeout=5s"
     ];
   };
-
+  services.dbus = {
+    enable = true;
+    implementation = "broker";
+  };
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
