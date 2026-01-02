7909ae91c6e614fee847d22f1b59a8231a18c163
Michal Gawronski-Kot <michal@gawronskikot.com>
2026-01-02 16:03:36 +0000

    hopying to fix chrome icon showing twice in taskbar

--- modules/apps/google-chrome/default.nix
+++ modules/apps/google-chrome/default.nix
@@ -28,6 +28,5 @@ in
     environment.etc."/opt/chrome/policies/enrollment/CloudManagementEnrollmentToken".source = config.age.secrets.chrome-enrolment.path;
     environment.etc."/opt/chrome/policies/enrollment/CloudManagementEnrollmentOptions".text = "Mandatory";
     environment.sessionVariables.NO_AT_BRIDGE = "1";
-    environment.etc."xdg/applications/google-chrome.desktop".source = ./google-chrome.desktop;
   };
 }
