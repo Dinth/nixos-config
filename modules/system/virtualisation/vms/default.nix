0bb23bff4c5ce5ebd24d4788a78c195d90751dc5
Michal Gawronski-Kot <michal@gawronskikot.com>
2026-03-04 15:31:35 +0000

    nixvirt added

--- modules/system/virtualisation/vms/default.nix
+++ modules/system/virtualisation/vms/default.nix
@@ -1,18 +1,29 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, options, ... }:
+# NixVirt declarative VM management
+#
+# WARNING: When domains are defined here, NixVirt operates in fully declarative mode.
+# Any libvirt domain NOT declared in Nix will be DELETED on rebuild
+# (disk images are preserved, but VM definitions are removed).
 let
-  inherit (lib) mkIf;
-  cfg = config.virtualisation;
+  inherit (lib) mkIf elem optionalAttrs;
+  hostname = config.networking.hostName;
+
+  # All VMs run on desktop only
+  runOnHosts = [ "dinth-nixos-desktop" ];
+
+  # Check if NixVirt module is loaded
+  hasNixVirt = options ? virtualisation && options.virtualisation ? libvirt;
 in
 {
-  imports = [
-    # Import individual VM definitions here
-    # ./my-vm.nix
-  ];
-
-  config = mkIf cfg.enable {
+  config = optionalAttrs hasNixVirt (mkIf (config.virtualisation.enable && elem hostname runOnHosts) {
     virtualisation.libvirt.connections."qemu:///system" = {
-      # Domains, networks, and pools are defined in individual VM files
-      # and merged here via the module system
+      domains = [
+        { definition = ./win11.xml; }
+        { definition = ./win10.xml; }
+        { definition = ./macos.xml; }
+        { definition = ./linuxmint.xml; }
+        { definition = ./kali.xml; }
+      ];
     };
-  };
+  });
 }
