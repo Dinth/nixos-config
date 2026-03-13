a4796f522b3db9394475e1366078c630af3e00c8
Michal Gawronski-Kot <michal@gawronskikot.com>
2026-03-13 21:14:29 +0000

    Improve Logitech MX Master 3 support

--- modules/hardware/logitech/default.nix
+++ modules/hardware/logitech/default.nix
@@ -21,18 +21,31 @@ in
       wireless.enableGraphical = true;
     };
     environment.systemPackages = with pkgs; [
-      logiops # Unofficial HID driver for Logitech devices
+      logiops
     ];
     systemd.services.logiops = {
       description = "An unofficial userspace driver for HID++ Logitech devices";
       wantedBy = [ "graphical.target" ];
-      after = [ "bluetooth.target" ];
+      after = [ "multi-user.target" ];
       serviceConfig = {
         ExecStart = "${lib.getExe pkgs.logiops}";
+        Restart = "on-failure";
+        RestartSec = "3";
       };
       restartTriggers = [ config.environment.etc."logid.cfg".source ];
     };
     environment.etc."logid.cfg".source = ./logid.cfg;
+
+    # Restart logiops when Logitech USB receiver connects
+    services.udev.extraRules = ''
+      ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="046d", TAG+="systemd", ENV{SYSTEMD_WANTS}="logiops.service"
+    '';
+
+    # Mouse-specific libinput settings
+    services.libinput.mouse = {
+      accelProfile = "flat";
+      accelSpeed = "0";
+    };
   };
 }
 
