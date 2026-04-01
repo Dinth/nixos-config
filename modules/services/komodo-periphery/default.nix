8757192ff0a29a3c804fe982a1236ba43abf23d6
Michal Gawronski-Kot <michal@gawronskikot.com>
2026-04-01 09:06:01 +0100

    komodo-periphery: add openssl to PATH for SSL cert generation

--- modules/services/komodo-periphery/default.nix
+++ modules/services/komodo-periphery/default.nix
@@ -25,6 +25,7 @@ let
       makeWrapper ${pkgs.nix-ld}/libexec/nix-ld $out/bin/periphery \
         --set NIX_LD_LIBRARY_PATH "${lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib pkgs.openssl ]}" \
         --set NIX_LD "${pkgs.stdenv.cc.libc}/lib/ld-linux-x86-64.so.2" \
+        --prefix PATH : "${lib.makeBinPath [ pkgs.openssl ]}" \
         --add-flags "$out/bin/.periphery-unwrapped"
     '';
   };
