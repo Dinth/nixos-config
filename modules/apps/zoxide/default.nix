3d068397355f783d516606f480367a4c32be8b3f
Michal Gawronski-Kot <michal@gawronskikot.com>
2026-05-08 21:38:38 +0100

    fix(zoxide): re-pin chpwd hook last to silence init-order warning

--- modules/apps/zoxide/default.nix
+++ modules/apps/zoxide/default.nix
@@ -3,7 +3,7 @@
   lib,
   ...
 }: let
-  inherit (lib) mkIf;
+  inherit (lib) mkIf mkAfter;
   cfg = config.cli;
   primaryUsername = config.primaryUser.name;
 in {
@@ -14,6 +14,15 @@ in {
         enable = true;
         enableZshIntegration = true;
       };
+      # HM inits zoxide at order 851 and starship at 1000, so starship_chpwd
+      # ends up appended to chpwd_functions after __zoxide_hook and zoxide
+      # warns that it wasn't initialized last. Re-pin __zoxide_hook to the
+      # end of chpwd_functions after both integrations have run.
+      programs.zsh.initContent = mkAfter ''
+        if (( ''${+chpwd_functions} )) && (( ''${chpwd_functions[(I)__zoxide_hook]} )); then
+          chpwd_functions=(''${chpwd_functions:#__zoxide_hook} __zoxide_hook)
+        fi
+      '';
     };
   };
 }
